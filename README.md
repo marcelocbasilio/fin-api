10f52a53c7c1d3d555a2c61d6713fc8536f2727b
Marcelo Cordeiro Basílio <marcelocbasilio@gmail.com>
2021-04-12 11:51:07 -0300

    The withdrawal routine was carried out, analyzing whether the account in question has sufficient balance and whether it exists.

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 
 - [x] It should be possible to create an account
 - [x] It must be possible to retrieve the customer's bank statement
-- [] It must be possible to make a deposit
-- [] It must be possible to make a withdrawal
+- [x] It must be possible to make a deposit
+- [x] It must be possible to make a withdrawal
 - [] It must be possible to retrieve the customer's bank statement by date
 - [] It must be possible to update customer account data
 - [] It must be possible to obtain customer account data
@@ -19,10 +19,11 @@
 
 - [x] It should not be possible to register an account with an existing CPF
 - [x] It should not be possible to deposit to a non-existing account
-- [] It should not be possible to fetch a statement from a non-existing account
-- [] It should not be possible to withdraw to a non-existing account
+- [x] It should not be possible to fetch a statement from a non-existing account
+- [x] It should not be possible to withdraw to a non-existing account
+- [x] It should not be possible to withdraw when the balance is insufficient
 - [] It should not be possible to delete a non-existing account
-- [] It should not be possible to withdraw when the balance is insufficient
+
 
 ---
 
--- src/index.js
+++ src/index.js
@@ -21,6 +21,19 @@ function verifyIfExistsAccountCpf(request, response, next) {
     return next()
 }
 
+//
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === "credit") {
+            return acc + operation.amount
+        } else {
+            return acc - operation.amount
+        }
+    }, 0)
+
+    return balance
+}
+
 // Routes
 app.post("/account", (request,response) => {
     const { cpf, name } = request.body
@@ -45,6 +58,43 @@ app.get("/statement", verifyIfExistsAccountCpf, (request, response) => {
     const { customer } = request
 
     return response.json(customer.statement)
+})
+
+app.post("/deposit", verifyIfExistsAccountCpf, (request, response) => {
+
+    const { description, amount } = request.body
+    const { customer } = request
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit"
+    }
+
+    customer.statement.push(statementOperation)
+
+    return response.status(201).send()
+})
+
+app.post("/withdraw", verifyIfExistsAccountCpf, (request, response) => {
+    
+    const { amount } = request.body
+    const { customer } = request
+    const balance = getBalance(customer.statement)
+
+    if (balance < amount) {
+        return response.status(400).json({error: "Insufficient funds!"})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
+
+    customer.statement.push(statementOperation)
+
+    return response.status(201).send()
 })
 
 app.listen(3333)
